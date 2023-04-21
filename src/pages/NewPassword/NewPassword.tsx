1eaf3e35bf1f4d5274f7a65ba852e3f4f164617e
Maaike Hek <maaikehek@hotmail.com>
2023-04-21 17:10:46 +0200

    feat(new-password): add basic newpassword form

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCustomers } from "../../helpers";
+import { Customer } from "../../types";
 
 export const NewPassword = () => {
-  return <div>new password</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [customers, setCustomers] = useState<Customer[] | undefined>();
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      console.log("hoi");
+      const data = await getCustomers();
+
+      setIsLoading(false);
+      setCustomers(data);
+    };
+
+    fetchCustomers();
+  });
+
+  return (
+    <div>
+      <h1>Add new password</h1>
+      {isLoading ? (
+        "Loading"
+      ) : (
+        <form>
+          <label>
+            Title*
+            <input name="title" required></input>
+          </label>
+          <label>
+            Password*
+            <input name="title" type="password" required></input>
+          </label>
+          {customers && (
+            <label>
+              Customer
+              <select>
+                {customers.map(({ name }) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          )}
+        </form>
+      )}
+    </div>
+  );
 };
